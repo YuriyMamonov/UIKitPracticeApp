7878eceec953be472a594f951cdf64ac70685577
Yuriy <178mamonov@mail.ru>
2020-02-12 20:05:24 +0300

    Initial Commit

---
+++ UIKitPracticeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKitPracticeApp
+//
+//  Created by Юрий Мамонов on 12.02.2020.
+//  Copyright © 2020 Yuriy Mamonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
